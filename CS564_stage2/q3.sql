98cc14e6abd592715d91c073c9c952281e1d5439
Sheehyun Kim <sheehyun@localhost.localdomain>
2024-10-06 22:25:19 -0700

    queries 1,2,3 done

---
+++ CS564_stage2/q3.sql
@@ -0,0 +1,1 @@
+SELECT department_name, AVG(jobs.max_salary) FROM departments JOIN employees ON employees.department_id = departments.department_id JOIN jobs ON jobs.job_id = employees.job_id GROUP BY department_name HAVING AVG(jobs.max_salary) > 8000;
