3efab14384eca9f44b700509b91bbb25d80df00a
mukundkk <mukund.krishnakumar@gmail.com>
2024-10-07 15:09:47 -0500

    q7,8,9

---
+++ CS564_stage2/q8.sql
@@ -0,0 +1,1 @@
+SELECT e.manager_id, e.salary FROM employees e WHERE e.salary = (SELECT MIN(salary) FROM employees WHERE manager_id IS NOT NULL);
---
+++ CS564_stage2/q9.sql
@@ -0,0 +1,1 @@
+SELECT d.department_name FROM departments d JOIN employees e ON d.department_id = e.department_id ORDER BY e.salary DESC LIMIT 1;
