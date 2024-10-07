00afb7535fdfbf32600f65846c9f032fcd8b1eba
charliebharlie <charlie.liu@warriorlife.net>
2024-10-06 23:58:04 -0500

    Fixed q3

--- q3.sql
+++ q3.sql
@@ -1,1 +1,1 @@
-SELECT e.first_name, j.max_salary FROM employees e, departments d, jobs j WHERE e.department_id = d.department_id and e.job_id = j.job_id GROUP BY d.department_id HAVING AVG(j.max_salary) > 8000;
+SELECT d.department_name, AVG(j.max_salary) FROM employees e, departments d, jobs j WHERE e.department_id = d.department_id and e.job_id = j.job_id GROUP BY d.department_id HAVING AVG(j.max_salary) > 8000;
