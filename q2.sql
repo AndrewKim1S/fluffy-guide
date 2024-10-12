e3d03234303ec28e8b2c37eac7fd6e878854afa7
Sheehyun Kim <sheehyun@localhost.localdomain>
2024-10-11 18:02:09 -0700

    finished

--- q2.sql
+++ q2.sql
@@ -1,1 +1,1 @@
-SELECT department_name, COUNT(employee_id) FROM departments LEFT JOIN employees ON employees.department_id = departments.department_id GROUP BY department_name ORDER BY COUNT(employee_id);
+SELECT department_name, COUNT(employee_id) FROM departments LEFT JOIN employees ON employees.department_id = departments.department_id GROUP BY department_name ORDER BY COUNT(employee_id) DESC;
