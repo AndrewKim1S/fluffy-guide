769c62befc886937350f66fb469e19fad0a53852
Sheehyun Kim <sheehyun@localhost.localdomain>
2024-12-01 20:55:36 -0800

    insert almost done but C ptr cast error

--- Stage6/insert.C
+++ Stage6/insert.C
@@ -14,8 +14,78 @@ const Status QU_Insert(const string & relation,
 	const int attrCnt, 
 	const attrInfo attrList[])
 {
-// part 6
-return OK;
+    Status status;
 
+    // Get the total number of attributes
+    int attrCntParameter = 0;
+    AttrDesc *attrDescArray;
+    status = attrCat->getRelInfo(relation, attrCntParameter, attrDescArray);
+    if(status != OK) { return status; }
+
+    // Check if the attributes number are equal - minirel rejects NULLs
+    if(attrCntParameter != attrCnt) { return ATTRNOTFOUND; }
+
+    // Find reclen
+    int reclen = 0;
+    for(int i = 0; i < attrCnt; i++) {
+        reclen += attrDescArray[i].attrLen;
+    }
+
+    // Create record to insert
+    Record outputRec;
+    char *outputData[reclen];
+    outputRec.data = (void*) outputData;
+    outputRec.length = reclen;
+
+    // tmp vars for type issues
+    int int_tmp;
+    float float_tmp;
+
+    // n^2 search to match attrList arguments to attrDescArray given by RelInfo - (sort them)
+    int outputOffset = 0;
+    for(int i = 0; i < attrCnt; i++) {
+        bool found = false;
+        for(int j = 0; j < attrCnt; j++) {
+            
+            // attributes match
+            if(strcmp(attrDescArray[i].attrName, attrList[j].attrName) == 0) {
+                found = true;
+                outputOffset = attrDescArray[i].attrOffset;
+
+                // Check the the type cause else it cries and segfaults
+                // attrList len is -1??
+                switch(attrDescArray[i].attrType) {
+                    // copy char* to data
+                    // TODO There is an error in how I am copying the string I believe. fuck c syntax man 
+                    case 0:
+                        memcpy(outputData + outputOffset, (char*)attrList[j].attrValue, attrDescArray[i].attrLen);
+                        break;
+                    // copy integer to data
+                    case 1:
+                        int_tmp = atoi((char*)attrList[j].attrValue);
+                        memcpy(outputData + outputOffset, (char*)&int_tmp, attrDescArray[i].attrLen);
+                        break;
+                    // copy float to data
+                    case 2:
+                        float_tmp = atof((char*)attrList[j].attrValue);
+                        memcpy(outputData + outputOffset, (char*)&float_tmp, attrDescArray[i].attrLen);
+                        break;
+                }
+                break;
+            }
+        }
+        if(!found) { return ATTRNOTFOUND; } 
+    }
+
+    // open the result table 
+    InsertFileScan resultTable(relation, status);
+    if(status != OK) { return status; }
+
+    // insert record to table
+    RID outputRid;
+    status = resultTable.insertRecord(outputRec, outputRid);
+    if(status != OK) { return status; }
+
+    return OK;
 }
 
